9605c6f6f4e30a62a0034862d2655961d50003a9
DanieleCorrado <corrado_daniele@virgilio.it>
2023-04-28 17:35:28 +0200

    add style

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
 let Form = document.getElementById("form");
 let ticketPrice = 0;
+let offertTipe = "Tariffa base";
 const pricePerKilometre = 0.21;
 
 Form.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  onclick = document.getElementById("ticket-info").style.display = "block";
+
   let username = document.getElementById("username");
+
   let age = document.getElementById("age");
+
   let kilometersTravelled = document.getElementById("kilometers-travelled");
 
   ticketPrice = pricePerKilometre * kilometersTravelled.value;
@@ -14,15 +19,24 @@ Form.addEventListener("submit", (e) => {
   if (age.value < 18) {
 
     ticketPrice = parseFloat((ticketPrice - ((ticketPrice * 20) / 100)));
+    offertTipe = "Sconto minori"
 
-  } else if (userAge.value >= 65) {
+  } else if (age.value >= 65) {
 
     ticketPrice =parseFloat((ticketPrice - ((ticketPrice * 40) / 100)));
+    offertTipe = "Sconto over 65"
 
+  } else {
+    offertTipe = "Tariffa base"
   }
     console.log(username.value, age.value);
 
-    document.getElementById("ticket-price").innerHTML = `Il prezzo del biglietto è ${ticketPrice.toFixed(2)}` + '\u20AC';
+    document.getElementById("name-surname").innerHTML = username.value;
+
+    document.getElementById("offert-tipe").innerHTML = offertTipe;
+
+
+    document.getElementById("price").innerHTML = ticketPrice.toFixed(2) + '\u20AC';
   
 }
 );
