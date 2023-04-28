fc485058415ad64fdb4fb4269a50cd87ec1e3c75
DanieleCorrado <corrado_daniele@virgilio.it>
2023-04-28 16:00:14 +0200

    add ticket price structure

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-console.log("prova");
+let Form = document.getElementById("Form");
+let ticketPrice = 0;
+const pricePerKilometre = 0.21;
+
+Form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let username = document.getElementById("username");
+  let age = document.getElementById("age");
+  let kilometersTravelled = document.getElementById("kilometers-travelled");
+
+  ticketPrice = pricePerKilometre * kilometersTravelled.value;
+
+  if (age.value < 18) {
+
+    ticketPrice = parseFloat((ticketPrice - ((ticketPrice * 20) / 100)));
+
+  } else if (userAge.value >= 65) {
+
+    ticketPrice =parseFloat((ticketPrice - ((ticketPrice * 40) / 100)));
+
+  }
+    console.log(username.value, age.value);
+
+    document.getElementById("ticket-price").innerHTML = `Il prezzo del biglietto è ${ticketPrice.toFixed(2)}`;
+  
+});
